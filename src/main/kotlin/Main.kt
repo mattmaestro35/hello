c74672047d577aafce93f330354e72e2dcd551f5
mattmaestro35 <mlarson7@illinois.edu>
2020-02-21 17:08:49 -0600

    Updated to halfway through Wed 2/19 lecture

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
 fun hello(): String {
     return "Hello, world!"
+}
+data class AddResult(val first: Int, val second: Int, val result: Int)
+// return an instance of AddResult
+
+fun main() {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello, world!")
+            }
+        }
+    }.start(wait = true)
 }
--- src/test/kotlin/TestMain.kt
+++ src/test/kotlin/TestMain.kt
@@ -1,8 +1,4 @@
-import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 
 class TestMain : StringSpec({
-    "should greet properly" {
-        hello() shouldBe "Hello, world!"
-    }
 })
